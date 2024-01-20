7b9906140c00ba8baca7988fab47ba8d404649d4
EvanGarcia16 <142777443+EvanGarcia16@users.noreply.github.com>
2024-01-20 13:59:39 +0800

    Add files via upload

--- database/management_system.sql
+++ database/management_system.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 19, 2024 at 12:51 AM
--- Server version: 8.0.31
--- PHP Version: 8.0.26
+-- Generation Time: Jan 20, 2024 at 05:41 AM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,7 +45,8 @@ INSERT INTO `accounts` (`UserID`, `Password`, `Password_Status`, `Account_Status
 (1, '$2y$10$uDuS1bXRCNVWydmkMPdKROpmQNrsE3qzXTDcQY/tZ1N9Lgw6tYKre', 0, 'Active', 'Offline'),
 (2, '$2y$10$PFEnW38HX42Ow3XtqSl5Y.daRyOXAoLUZkL.6uW3xAhvvdwHbssQq', 0, 'Active', 'Offline'),
 (3, '$2y$10$rqVEXJbmEYY6q40EA6o6Lej3TMt3IvHA2nXH4T8m8Qu3WZhVBcaIe', 0, 'Active', 'Offline'),
-(4, '$2y$10$9ErEld0e/UrHPWEf/QPfae2Uh/j.aZFAyscdRR6i.o7bas/cUTPsW', 0, 'Active', 'Offline');
+(4, '$2y$10$9ErEld0e/UrHPWEf/QPfae2Uh/j.aZFAyscdRR6i.o7bas/cUTPsW', 0, 'Active', 'Offline'),
+(5, '$2y$10$nBFuSOS4qAbZg92qb23yDevvj25cUF.UvYmzSGXUyU/ou0YWOhKti', 0, 'Active', 'Offline');
 
 -- --------------------------------------------------------
 
@@ -70,7 +71,8 @@ INSERT INTO `slu` (`IDNumber`) VALUES
 (2220016),
 (2220848),
 (2220161),
-(2227003);
+(2227003),
+(2222783);
 
 -- --------------------------------------------------------
 
@@ -103,7 +105,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `Program` varchar(4) NOT NULL,
   `Role` varchar(10) NOT NULL,
   PRIMARY KEY (`UserID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
@@ -113,7 +115,8 @@ INSERT INTO `users` (`UserID`, `IDNum`, `FirstName`, `LastName`, `Program`, `Rol
 (1, '2227828', 'Justin', 'Montemayor', 'BSIT', 'Student'),
 (2, '2221853', 'Alkaid', 'Pading', 'BSCS', 'Student'),
 (3, '2220848', 'Angela', 'Ramat', 'BMMA', 'Student'),
-(4, '2222484', 'Carl', 'Pascua', 'BSIT', 'Student');
+(4, '2222484', 'Carl', 'Pascua', 'BSIT', 'Student'),
+(5, '2222783', 'Evan', 'Garcia', 'BSIT', 'Student');
 
 -- --------------------------------------------------------
 
@@ -128,10 +131,17 @@ CREATE TABLE IF NOT EXISTS `utilization` (
   `Date` date NOT NULL,
   `Time` time NOT NULL,
   `Room` varchar(30) NOT NULL,
-  `Purpose` int NOT NULL,
+  `Purpose` varchar(255) NOT NULL,
   PRIMARY KEY (`UtilizationID`),
   KEY `user_util` (`UserID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `utilization`
+--
+
+INSERT INTO `utilization` (`UtilizationID`, `UserID`, `Date`, `Time`, `Room`, `purpose`) VALUES
+(3, 5, '2024-01-20', '07:29:00', 'D513', 'school');
 
 --
 -- Constraints for dumped tables
