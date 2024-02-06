6ccdbde5d3aecf4fc4423b8cd5adbf7475c92515
Gerald Angway <angwaygerald6@gmail.com>
2024-02-06 19:41:23 +0800

    2x2 with c2 syampyon

--- admin_module/admin_userlogs.php
+++ admin_module/admin_userlogs.php
@@ -27,38 +27,58 @@
 
         <br><br>
 
+        <div class="search">
+            <form method="get" action="admin_userlogs.php">
+                <label for="idNumber">Search by ID Number:</label>
+                <input type="text" id="idNumber" name="idNumber">
+                <button type="submit">Search</button>
+            </form>
+        </div>
+
         <div class="table">
             <table>
                 <thead>
                     <tr>
-                        <th>Log ID</th>
-                        <th>User ID</th>
-                        <th>Action</th>
-                        <th>Timestamp</th>
+                        <th>Utilization ID</th>
+                        <th>Name</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Room</th>
+                        <th>Purpose</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <?php
+                <?php
                     require('../database/db.php');
 
-                    $logsSql = "SELECT * FROM userlogs";
-                    $logsResult = $db->query($logsSql);
+                    // Check if a search query is submitted
+                    if(isset($_GET['idNumber'])) {
+                        $searchID = $_GET['idNumber'];
+                        $logsSql = "SELECT u.UtilizationID, u.UserID, u.Date, u.Time, u.Room, u.Purpose, us.IDNum FROM utilization u JOIN users us ON u.UserID = us.UserID WHERE us.IDNum = '$searchID'";
+                        $logsResult = $db->query($logsSql);
+                    } else {
+                        // If no search query, fetch all logs
+                        $logsSql = "SELECT u.UtilizationID, u.UserID, u.Date, u.Time, u.Room, u.Purpose, us.IDNum FROM utilization u JOIN users us ON u.UserID = us.UserID";
+                        $logsResult = $db->query($logsSql);
+                    }
 
                     if ($logsResult && $logsResult->num_rows > 0) {
                         while ($logRow = $logsResult->fetch_assoc()) {
                             echo "<tr>
-                                    <td>{$logRow['LogID']}</td>
-                                    <td>{$logRow['UserID']}</td>
-                                    <td>{$logRow['Action']}</td>
-                                    <td>{$logRow['Timestamp']}</td>
+                                    <td>{$logRow['UtilizationID']}</td>
+                                    <td>{$logRow['IDNum']}</td>
+                                    <td>{$logRow['Date']}</td>
+                                    <td>{$logRow['Time']}</td>
+                                    <td>{$logRow['Room']}</td>
+                                    <td>{$logRow['Purpose']}</td>
                                 </tr>";
                         }
                     } else {
-                        echo "<tr><td colspan='4'>No User Logs</td></tr>";
+                        echo "<tr><td colspan='6'>No User Logs</td></tr>";
                     }
 
                     $db->close();
-                    ?>
+                ?>
                 </tbody>
             </table>
         </div>
