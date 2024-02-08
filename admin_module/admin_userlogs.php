b938380d5aa40836e0a402eab942356eeb5493a9
Gerald Angway <angwaygerald6@gmail.com>
2024-02-08 23:22:26 +0800

    users logs added the name

--- admin_module/admin_userlogs.php
+++ admin_module/admin_userlogs.php
@@ -1,3 +1,38 @@
+<?php
+require('../database/db.php');
+require('../otherpages/require_session.php');
+
+$userID = isset($_SESSION['userID']) ? $_SESSION['userID'] : null;
+$name = "";
+
+if ($userID !== null) {
+    $sqlUser = "SELECT CONCAT(FirstName, ' ', LastName) AS FullName FROM users WHERE UserID = ?";
+    $stmtUser = $db->prepare($sqlUser);
+    $stmtUser->bind_param("i", $userID);
+    $stmtUser->execute();
+    $resultUser = $stmtUser->get_result();
+
+    if ($rowUser = $resultUser->fetch_assoc()) {
+        $name = $rowUser['FullName'];
+    }
+
+    $stmtUser->close();
+}
+
+if (isset($_GET['idNumber'])) {
+    $searchID = $_GET['idNumber'];
+    $logsSql = "SELECT u.UtilizationID, u.UserID, u.Date, u.Time, u.Room, u.Purpose, us.IDNum, CONCAT(us.FirstName, ' ', us.LastName) AS UserName FROM utilization u JOIN users us ON u.UserID = us.UserID WHERE us.IDNum = ?";
+    $stmtLogs = $db->prepare($logsSql);
+    $stmtLogs->bind_param("s", $searchID);
+    $stmtLogs->execute();
+    $logsResult = $stmtLogs->get_result();
+} else {
+    $logsSql = "SELECT u.UtilizationID, u.UserID, u.Date, u.Time, u.Room, u.Purpose, us.IDNum, CONCAT(us.FirstName, ' ', us.LastName) AS UserName FROM utilization u JOIN users us ON u.UserID = us.UserID";
+    $logsResult = $db->query($logsSql);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,103 +42,72 @@
     <link rel="stylesheet" href="../css/userlogs.css">
 </head>
 <body>
-    <div class="box-container">
+<div class="box-container">
 
-        <div class="logout">
-            <a href='../otherpages/logout.php'>Logout</a>
-        </div>
+    <div class="logout">
+        <a href='../otherpages/logout.php'>Logout</a>
+    </div>
 
-        <div class="reset">
-            <a href='admin.php'>Reset Password Request/s</a>
-        </div>
+    <div class="reset">
+        <a href='admin.php'>Reset Password Request/s</a>
+    </div>
 
-        <div class="users">
-            <a href='admin_viewusers.php'>Users</a>
-        </div>
+    <div class="users">
+        <a href='admin_viewusers.php'>Users</a>
+    </div>
 
-        <div class="logs">
-            <a href='admin_userlogs.php'>User Logs</a>
-        </div>
+    <div class="logs">
+        <a href='admin_userlogs.php'>User Logs</a>
+    </div>
 
-        <br><br>
+    <br><br>
 
-        <div class="search">
-            <form method="get" action="admin_userlogs.php">
-                <input type="text" id="idNumber" name="idNumber" placeholder="Search ID Number">
-                <button type="submit">Search</button>
-            </form>
-        </div>
+    <div class="search">
+        <form method="get" action="admin_userlogs.php">
+            <input type="text" id="idNumber" name="idNumber" placeholder="Search ID Number">
+            <button type="submit">Search</button>
+        </form>
+    </div>
 
-        <br>
+    <br>
 
-        <div class="table">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Utilization ID</th>
-                        <th>ID Number</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                        <th>Room</th>
-                        <th>Purpose</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
-                    require('../database/db.php');
-                    require('../otherpages/require_session.php');
-
-                    $userID = isset($_SESSION['userID']) ? $_SESSION['userID'] : null;
-                            $name = "";
-                            $role = "";
-
-                            if ($userID !== null) {
-                                $sqlUser = "SELECT CONCAT(FirstName, ' ', LastName) AS FullName, Role FROM users WHERE UserID = ?";
-                                $stmtUser = $db->prepare($sqlUser);
-                                $stmtUser->bind_param("i", $userID);
-                                $stmtUser->execute();
-                                $resultUser = $stmtUser->get_result();
-
-                                if ($rowUser = $resultUser->fetch_assoc()) {
-                                    $name = $rowUser['FullName'];
-                                    $role = $rowUser['Role'];
-                                }
-
-                                $stmtUser->close();
-                            }
-
-                    if(isset($_GET['idNumber'])) {
-                        $searchID = $_GET['idNumber'];
-                        $logsSql = "SELECT u.UtilizationID, u.UserID, u.Date, u.Time, u.Room, u.Purpose, us.IDNum FROM utilization u JOIN users us ON u.UserID = us.UserID WHERE us.IDNum = '$searchID'";
-                        $logsResult = $db->query($logsSql);
-                    } else {
-                        $logsSql = "SELECT u.UtilizationID, u.UserID, u.Date, u.Time, u.Room, u.Purpose, us.IDNum FROM utilization u JOIN users us ON u.UserID = us.UserID";
-                        $logsResult = $db->query($logsSql);
-                    }
-
-                    if ($logsResult && $logsResult->num_rows > 0) {
-                        while ($logRow = $logsResult->fetch_assoc()) {
-                            echo "<tr>
+    <div class="table">
+        <table>
+            <thead>
+            <tr>
+                <th>Utilization ID</th>
+                <th>ID Number</th>
+                <th>Name</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Room</th>
+                <th>Purpose</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            if ($logsResult && $logsResult->num_rows > 0) {
+                while ($logRow = $logsResult->fetch_assoc()) {
+                    echo "<tr>
                                     <td>{$logRow['UtilizationID']}</td>
                                     <td>{$logRow['IDNum']}</td>
+                                    <td>{$logRow['UserName']}</td>
                                     <td>{$logRow['Date']}</td>
                                     <td>{$logRow['Time']}</td>
                                     <td>{$logRow['Room']}</td>
                                     <td>{$logRow['Purpose']}</td>
                                 </tr>";
-                        }
-                    } else {
-                        echo "<tr><td colspan='6'>No User Logs</td></tr>";
-                    }
-
-                    $db->close();
-                ?>
-                </tbody>
-            </table>
-        </div>
-
-<p><strong>Admin <?php echo $name; ?></strong></p>
-
+                }
+            } else {
+                echo "<tr><td colspan='7'>No User Logs</td></tr>";
+            }
+            ?>
+            </tbody>
+        </table>
     </div>
+
+    <p><strong>Admin <?php echo $name; ?></strong></p>
+
+</div>
 </body>
 </html>
